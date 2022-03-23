30abb7e145f1ac78841c2b66e81361f8cd175469
Bag132 <victortaco132@gmail.com>
2022-03-22 23:55:30 -0400

    Basic vote functionality

--- main.py
+++ main.py
@@ -4,52 +4,57 @@ import time
 preflight_url = 'https://api.urbandictionary.com/v0/vote'
 preflight_headers = {
 	'authority': 'api.urbandictionary.com',
-	'method': 'OPTIONS',
-	'path': '/v0/vote',
-	'scheme': 'https',
 	'accept': '*/*',
-	'accept-encoding': 'gzip, deflate, br',
-	'accept-language': 'en-US,en;q=0.9',
-	'access-control-request-headers': 'content-type',
 	'access-control-request-method': 'POST',
+	'access-control-request-headers': 'content-type',
 	'origin': 'https://www.urbandictionary.com',
-	'referer': 'https://www.urbandictionary.com/',
-	'sec-fetch-dest': 'empty',
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
 	'sec-fetch-mode': 'cors',
 	'sec-fetch-site': 'same-site',
-	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'
+	'sec-fetch-dest': 'empty',
+	'referer': 'https://www.urbandictionary.com/',
+	'accept-language': 'en-US,en;q=0.9',
 }
 
-vote_payload = '{defid: 16423887, direction: "down"} '
+vote_payload = '{"defid": 16423887, "direction": "down"} '
 vote_url = 'https://api.urbandictionary.com/v0/vote'
 vote_headers = {
 	'authority': 'api.urbandictionary.com',
-	'method': 'OPTIONS',
-	'path': '/v0/vote',
-	'scheme': 'https',
-	'accept': '*/*',
-	'accept-encoding': 'gzip, deflate, br',
-	'accept-language': 'en-US,en;q=0.9',
-	'content-length': str(len(vote_payload)),
-	'content-type': 'application/json; charset=utf-8',
-	'cookie': '_ga=GA1.2.929289410.1647897903; _gid=GA1.2.1279202208.1647897903; _li_dcdm_c=.urbandictionary.com; _lc2_fpi=7b1bfe6a19f7--01fyq5x06490ppx3c73b0cdrbv; cto_bundle=6pTutl9wVVNWZXN5UWdMMHpmWkhsJTJCZEdhaUwlMkZhSmFZaG5ERG4za3R4Y0xVOUNYenVWV0xqSVFld3JDcUlmN21teHNsdVRRZnZ5cGVWSjBGRzNkczJ5c3FGQlBWbUtxVSUyRkNIQjZKSTBJVVBGZDFwYUtCbXQxWldjdlJhbTd0VEc4Y1FrSndMVVBaMHglMkZWQTFIT09FWjIzQmhzZzNuUnhqcXBuR0tsdGtxQmhESmxhRSUzRA',
-	'dnt': '1',
-	'origin': 'https://www.urbandictionary.com',
-	'referer': 'https://www.urbandictionary.com/',
 	'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+	'dnt': '1',
 	'sec-ch-ua-mobile': '?0',
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
 	'sec-ch-ua-platform': '"Windows"',
-	'sec-fetch-dest': 'empty',
-	'sec-fetch-mode': 'cors',
+	'content-type': 'application/json; charset=utf-8',
+	'accept': '*/*',
+	'origin': 'https://www.urbandictionary.com',
 	'sec-fetch-site': 'same-site',
-	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'
+	'sec-fetch-mode': 'cors',
+	'sec-fetch-dest': 'empty',
+	'referer': 'https://www.urbandictionary.com/',
+	'accept-language': 'en-US,en;q=0.9',
+	# Requests sorts cookies= alphabetically
+	# 'cookie': '_ga=GA1.2.929289410.1647897903; _li_dcdm_c=.urbandictionary.com; _lc2_fpi=7b1bfe6a19f7--01fyq5x06490ppx3c73b0cdrbv; _gid=GA1.2.1560578189.1648003577; __qca=I0-1493211718-1648004540664; cto_bundle=oSTGYF9wVVNWZXN5UWdMMHpmWkhsJTJCZEdhaUQlMkY4T3JrYzlnUUtVJTJCc1JIYlRad0JVJTJGcTc3REdvemhJa1EwZHBrS2NpbHZTYVl0YWExWXNXYXFMU2l4SmVwTHVqMUpGVmtEd0V1R3N2NUwlMkZFeUFISzREZ3FNNnRORERIbXZBMSUyQnBRNWFQWkFxaGNuRjdKRjlEVUtDUzZwb0FYWHB1d2dmclMxVzVuR0hYVVdpWXlQbWslM0Q',
 }
 vote_cookie = {
-	'_li_dcdm_c=': '.urbandictionary.com',
+	'_ga': 'GA1.2.929289410.1647897903',
+	'_li_dcdm_c': '.urbandictionary.com',
+	'_lc2_fpi': '7b1bfe6a19f7--01fyq5x06490ppx3c73b0cdrbv',
+	'_gid': 'GA1.2.1560578189.1648003577',
+	'__qca': 'I0-1493211718-1648004540664',
+	'cto_bundle': 'oSTGYF9wVVNWZXN5UWdMMHpmWkhsJTJCZEdhaUQlMkY4T3JrYzlnUUtVJTJCc1JIYlRad0JVJTJGcTc3REdvemhJa1EwZHBrS2NpbHZTYVl0YWExWXNXYXFMU2l4SmVwTHVqMUpGVmtEd0V1R3N2NUwlMkZFeUFISzREZ3FNNnRORERIbXZBMSUyQnBRNWFQWkFxaGNuRjdKRjlEVUtDUzZwb0FYWHB1d2dmclMxVzVuR0hYVVdpWXlQbWslM0Q',
 }
 if __name__ == '__main__':
+	session = requests.Session()
+
 	# send preflight
-	print(requests.options(preflight_url, headers=preflight_headers).text)
-	# time.sleep(0.5)
+	start_time = time.time()
+	preflight_response = session.options(preflight_url, headers=preflight_headers)
+	print("Time after preflight: " + str(time.time() - start_time))
+	print('Preflight Headers: ' + str(preflight_response.headers.values()))
+	print('Preflight Cookies: ' + str(preflight_response.cookies.get_dict()))
+	print('Preflight text: ' + str(preflight_response.text))
 	# send vote
-	print(requests.post(vote_url, headers=vote_headers).text)
+	vote_response = session.post(vote_url, headers=vote_headers, cookies=vote_cookie, data=vote_payload, timeout=5)
+	print(vote_response.text)
+	print("Time after vote: " + str(time.time() - start_time))
